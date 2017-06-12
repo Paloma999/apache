e901a1d8b24dfe597852fffa9caf130097eeb6c4
Paloma <paloma@gmail.com>
2017-06-12 11:36:13 +0200

    version 0.2.0

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,7 +6,11 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-	content '<h1>hello, world!</h1>'
+	content "<h1>hello, world!</h1>
+<h2>ipaddress: #{node['ipaddress']}</h2>
+<h2>hostname: #{node['hostname']}</h2>
+"
+
 end
 
 service 'httpd' do 
